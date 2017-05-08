735438bfb622085971333f1d367327bc1912de1a
Emmas <emmas1900@hotmail.com>
2017-05-08 15:08:48 -0500

    modificando congif en puerto y localhost

--- Config/config.js
+++ Config/config.js
@@ -1,6 +1,6 @@
 module.exports={
-"IP":process.env.IP || '0.0.0.0',
-"PORT":process.env.PORT || 3000,
+"IP":process.env.IP,
+"PORT":process.env.PORT,
 "color_theme":{
     "info":"rainbow",
     "data": "green",
