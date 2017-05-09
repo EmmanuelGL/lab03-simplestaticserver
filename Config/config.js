cbd809ab4a293d487a03e68accba3bb557228933
Emmas <emmas1900@hotmail.com>
2017-05-09 00:02:41 -0500

    coneccion 2

--- Config/config.js
+++ Config/config.js
@@ -1,6 +1,8 @@
 module.exports={
-"IP":process.env.IP || '0.0.0.0',
-"PORT":process.env.PORT || 3000,
+ "IP": process.env.IP || '0.0.0.0',
+  "PORT":process.env.PORT || '3000',
+//"IP":process.env.IP || '0.0.0.0',
+//"PORT":process.env.PORT || 3000,
 "color_theme":{
     "info":"rainbow",
     "data": "green",
@@ -8,6 +10,6 @@ module.exports={
     "warning":"yellow"
 },
 "STATIC_PATH":"./static",
-"url": process.env.DB || "mongodb://condos:condos@ds133251.mlab.com:33251/condos" 
+"dbStringConnection": process.env.DB || "mongodb://condos:condos@ds133251.mlab.com:33251/condos" 
 
 };
